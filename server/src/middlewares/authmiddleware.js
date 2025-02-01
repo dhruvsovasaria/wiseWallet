899d143fe7babdafe3af5906464c77f1c60add27
Dhruv Sovasaria <dhruvsovasaria34@gmail.com>
2025-02-01 20:57:40 +0530

    fix : Add token check condition

--- server/src/middlewares/authmiddleware.js
+++ server/src/middlewares/authmiddleware.js
@@ -3,13 +3,19 @@ const { jwt } = require("jsonwebtoken");
 const authMiddleware = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status.json({
+        message: "authentication token missing",
+      });
+    }
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
+
     console.log(verified);
     next();
   } catch (error) {
     res.status(401).json({
-      message: "unauthorized ",
+      message: "unauthorized",
     });
   }
 };
